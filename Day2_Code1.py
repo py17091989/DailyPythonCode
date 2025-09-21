26f763b776fccbc610123b97d43d0181a5e9d67e
Devendra Kushwaha <deed89@gmail.com>
2025-09-21 10:54:39 +0530

    Add Day2_Code1.py git push -u origin main
    git commit -m Add

---
+++ Day2_Code1.py
@@ -0,0 +1,9 @@
+#Data and variables
+name="Devendra" #string-text data
+age=35 #int
+weight=88.67 #float
+is_student=True #Boolean (True/False)
+print("Name :",name,"/ Type ",type(name))
+print("age :",age,"/ Type ",type(age))
+print("Weight :",weight,"/ Type",type(weight))
+print("Is_student",is_student,"/ Type",type(is_student))
