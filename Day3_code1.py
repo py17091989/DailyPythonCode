a1f9c6982bc5bdf1219b13fa317ee0110cc8ddce
Devendra Kushwaha <deed89@gmail.com>
2025-09-22 18:39:09 +0530

    Improved prime number checker -Day3_code1

--- Day3_code1.py
+++ Day3_code1.py
@@ -6,7 +6,7 @@ if num>1:
     for i in range(2,num):
         if num%i==0:
             print(num,"is not a prime number")
-        break
+            break
     else:
          print(num,"is a prime number")
 
