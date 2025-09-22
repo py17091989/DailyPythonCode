f40ddd82068c3f84d85a8410b988fd06b6125b6d
Devendra Kushwaha <deed89@gmail.com>
2025-09-22 06:46:22 +0530

    Add Day3_code1

---
+++ Day3_code1.py
@@ -0,0 +1,12 @@
+#prime number checker
+#User se number input lena
+num=int(input("Enter any number :"))
+#prime number check logic
+if num>1:
+    for i in range(2,num):
+        if num%i==0:
+            print(num,"is not a prime number")
+        break
+    else:
+         print(num,"is a prime number")
+
